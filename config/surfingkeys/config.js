00906b6bae7a0532d7df61bf21f724d5f6573f06
Rob Hilgefort <rjhilgefort@gmail.com>
2021-12-29 11:58:09 -0700

    chore: fix surfing keys

--- config/surfingkeys/config.js
+++ config/surfingkeys/config.js
@@ -3,79 +3,103 @@
  * https://github.com/brookhong/Surfingkeys/blob/d96246d3a37abead7899d2b197b7f0dbe050cc95/pages/default.js#L258
  */
 
-const modes = {};
+// https://github.com/brookhong/Surfingkeys/wiki/Migrate-your-settings-from-0.9.74-to-1.0
+const {
+  aceVimMap,
+  mapkey,
+  imap,
+  imapkey,
+  getClickableElements,
+  vmapkey,
+  map,
+  unmap,
+  cmap,
+  addSearchAlias,
+  removeSearchAlias,
+  tabOpenLink,
+  readText,
+  Clipboard,
+  Front,
+  Hints,
+  Visual,
+  RUNTIME,
+  // RJH Added
+  Normal,
+} = api
+
+const modes = {}
 
 function addMode(mode, mapFn, mapkeyFn, unmapFn) {
-  if (typeof mode === "string") mode = { name: mode };
-  mode.mapFn = mapFn;
-  mode.mapkeyFn = mapkeyFn;
-  mode.unmapFn = unmapFn;
-  modes[mode.name] = mode;
+  if (typeof mode === 'string') mode = { name: mode }
+  mode.mapFn = mapFn
+  mode.mapkeyFn = mapkeyFn
+  mode.unmapFn = unmapFn
+  modes[mode.name] = mode
 }
 
 function addProvider(key, name, url, ...args) {
-  addSearchAliasX(key, name, url, ...args);
+  addSearchAliasX(key, name, url, ...args)
   mapkey(`o${key}`, `#8Open Search with alias ${key}`, () =>
-    Front.openOmnibar({ type: "SearchEngine", extra: key })
-  );
+    Front.openOmnibar({ type: 'SearchEngine', extra: key })
+  )
 }
 
 function mapp(mode, ...args) {
-  if (typeof mode === "string") {
-    args.unshift(mode);
-    mode = modes.Normal;
+  if (typeof mode === 'string') {
+    args.unshift(mode)
+    mode = modes.Normal
   }
-  if (args.length < 3) args.splice(1, 0, "");
-  mode.mapkeyFn(...args);
+  if (args.length < 3) args.splice(1, 0, '')
+  mode.mapkeyFn(...args)
 }
 
 function alias(mode, key, target, replace) {
-  if (typeof mode === "string") {
-    replace = target;
-    target = key;
-    key = mode;
-    mode = modes.Normal;
+  if (typeof mode === 'string') {
+    replace = target
+    target = key
+    key = mode
+    mode = modes.Normal
   }
-  mode.mapFn(key, target);
-  if (replace) unmapp(mode, target);
+  mode.mapFn(key, target)
+  if (replace) unmapp(mode, target)
 }
 
 function unmapp(mode, keys) {
   if (arguments.length === 1) {
-    keys = mode;
-    mode = modes.Normal;
+    keys = mode
+    mode = modes.Normal
   }
-  if (typeof keys === "string") keys = [keys];
-  for (const key of keys) mode.unmapFn(key);
+  if (typeof keys === 'string') keys = [keys]
+  for (const key of keys) mode.unmapFn(key)
 }
 
 function init() {
-  addMode(Normal, map, mapkey, unmap);
-  addMode(Insert, imap, imapkey, iunmap);
-  addMode(Visual, vmap, vmapkey, vunmap);
+  addMode(Normal, map, mapkey, unmap)
+  addMode(Insert, imap, imapkey, iunmap)
+  addMode(Visual, vmap, vmapkey, vunmap)
 }
 
 function doSettings() {
-  Hints.characters = "asdfghjkl;";
-  chrome.storage.local.set({ noPdfViewer: 1 });
-  settings.caseSensitive = true;
+  Hints.characters = 'asdfghjkl;'
+  chrome.storage.local.set({ noPdfViewer: 1 })
+  settings.caseSensitive = true
   // settings.smartCase = true;
 }
 
 function mappings() {
   // Disable surfing keys for page
-  map("<Ctrl-i>", "<Alt-s>");
+  map('<Ctrl-i>', '<Alt-s>')
 
   // TABS
   ////////////////////////////////////////////////////////
-  alias("dd", "x", true);
-  alias("gM", "<Alt-m>", true);
-  alias("gp", "<Alt-p>", true);
-  mapp(",r", "#4 Reload the page uncached", () =>
-    RUNTIME("reloadTab", { nocache: true })
-  );
-  alias("u", "X", true);
-  alias("<A-Tab>", "gt");
+  alias('dd', 'x', true)
+  alias('gM', '<Alt-m>', true)
+  alias('gp', '<Alt-p>', true)
+  mapp(',r', '#4 Reload the page uncached', () =>
+    RUNTIME('reloadTab', { nocache: true })
+  )
+  alias('u', 'X', true)
+  alias('<A-Tab>', 'gt')
   // TODO: move tabs more easily with "<Ctrl+{>"
   // alias("<Ctrl-{>", "E", true);
   // mapp("<Ctrl-Shift-[>", "#7 test 1", () => alert("<Ctrl-Shift-[>"));
@@ -89,15 +113,15 @@ function mappings() {
   //   alert("<Ctrl-{>");
   // });
 
-  mapp("p", "#7 Paste URL in current tab", () => {
-    Clipboard.read(({ data }) => (window.location.href = data));
-  });
-  mapp("P", "#7 Paste URL in new tab", () => {
-    Clipboard.read(({ data }) => tabOpenLink(data));
-  });
-  mapp("<Ctrl-p>", "#2 Unpin tab", () => {
-    RUNTIME("togglePinTab");
-  });
+  mapp('p', '#7 Paste URL in current tab', () => {
+    Clipboard.read(({ data }) => (window.location.href = data))
+  })
+  mapp('P', '#7 Paste URL in new tab', () => {
+    Clipboard.read(({ data }) => tabOpenLink(data))
+  })
+  mapp('<Ctrl-p>', '#2 Unpin tab', () => {
+    RUNTIME('togglePinTab')
+  })
 
   // SCROLLING
   ////////////////////////////////////////////////////////
@@ -107,12 +131,12 @@ function mappings() {
   // mapp("<Ctrl-e>", "#2 pageDown", () => Normal.scroll("pageDown"), {
   //   repeatIgnore: true,
   // });
-  mapp("<Ctrl-u>", "#2 pageUp", () => Normal.scroll("pageUp"), {
+  mapp('<Ctrl-u>', '#2 pageUp', () => Normal.scroll('pageUp'), {
     repeatIgnore: true,
-  });
-  mapp("<Ctrl-d>", "#2 pageDown", () => Normal.scroll("pageDown"), {
+  })
+  mapp('<Ctrl-d>', '#2 pageDown', () => Normal.scroll('pageDown'), {
     repeatIgnore: true,
-  });
+  })
   // mapp("<Ctrl-b>", "#2 pageUp", () => Normal.scroll("pageUp"), {
   //   repeatIgnore: true,
   // });
@@ -122,58 +146,58 @@ function mappings() {
 
   // HISTORY
   ////////////////////////////////////////////////////////
-  alias("H", "S", true);
-  alias("L", "D", true);
+  alias('H', 'S', true)
+  alias('L', 'D', true)
 
   // LINKS
-  alias("F", "af", true);
+  alias('F', 'af', true)
 
   // NAV
-  alias("st", "su", true);
-  mapkey("su", "#4 Edit current URL with vim editor (same tab)", () => {
+  alias('st', 'su', true)
+  mapkey('su', '#4 Edit current URL with vim editor (same tab)', () => {
     Front.showEditor(
       window.location.href,
       (url) => (window.location = url),
-      "url"
-    );
-  });
+      'url'
+    )
+  })
   mapkey(
-    "g#",
-    "#4Reload current page without hash string (all parts after #)",
+    'g#',
+    '#4Reload current page without hash string (all parts after #)',
     function () {
-      window.location.href = window.location.href.replace(/#[^\#]*$/, "");
+      window.location.href = window.location.href.replace(/#[^\#]*$/, '')
     }
-  );
+  )
 
   // INPUT
   ////////////////////////////////////////////////////////
-  mapp("a", "#1 Append to edit box", () => {
+  mapp('a', '#1 Append to edit box', () => {
     Hints.create(
-      "input:visible, textarea:visible, *[contenteditable=true], select:visible",
+      'input:visible, textarea:visible, *[contenteditable=true], select:visible',
       Hints.dispatchMouseClick
-    );
-    const element = document.activeElement;
+    )
+    const element = document.activeElement
     // contenteditable
     if (element.setSelectionRange === undefined) {
-      const selection = document.getSelection();
+      const selection = document.getSelection()
       selection.setPosition(
         selection.focusNode,
         selection.focusNode.data.length - 1
-      );
-      Visual.showCursor();
-      Visual.hideCursor();
-      return;
+      )
+      Visual.showCursor()
+      Visual.hideCursor()
+      return
     }
-    element.setSelectionRange(element.value.length, element.value.length);
-  });
-  alias("A", "I");
-  mapkey("ga", "#1 Go to last input", () =>
-    Hints.create("input[type=text]:visible:last", Hints.dispatchMouseClick)
-  );
-  alias(modes.Insert, "<Ctrl-o>", "<Ctrl-i>", true);
-  alias(modes.Insert, "<Ctrl-i>", "<Ctrl-f>", true);
-  alias(modes.Insert, "<Ctrl-h>", "<Alt-b>");
-  alias(modes.Insert, "<Ctrl-l>", "<Alt-f>");
+    element.setSelectionRange(element.value.length, element.value.length)
+  })
+  alias('A', 'I')
+  mapkey('ga', '#1 Go to last input', () =>
+    Hints.create('input[type=text]:visible:last', Hints.dispatchMouseClick)
+  )
+  alias(modes.Insert, '<Ctrl-o>', '<Ctrl-i>', true)
+  alias(modes.Insert, '<Ctrl-i>', '<Ctrl-f>', true)
+  alias(modes.Insert, '<Ctrl-h>', '<Alt-b>')
+  alias(modes.Insert, '<Ctrl-l>', '<Alt-f>')
 }
 
 function unmappings() {
@@ -183,15 +207,15 @@ function unmappings() {
 }
 
 function providers() {
-  addProvider("T", "twitch", "https://twitch.tv");
-  addProvider("r", "reddit", "https://www.reddit.com/r/");
-  addProvider("w", "wikipedia", "https://en.wikipedia.org/w/index.php?search=");
+  addProvider('T', 'twitch', 'https://twitch.tv')
+  addProvider('r', 'reddit', 'https://www.reddit.com/r/')
+  addProvider('w', 'wikipedia', 'https://en.wikipedia.org/w/index.php?search=')
 }
 
-init();
-doSettings();
-mappings();
+init()
+doSettings()
+mappings()
 // unmappings();
-providers();
+providers()
 
-unmapAllExcept([], /\/mail.google.com\//);
+unmapAllExcept([], /\/mail.google.com\//)
