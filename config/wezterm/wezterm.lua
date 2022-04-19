c2a33ad2e87fa5434116986f829ee389cfb2a1a9
Rob Hilgefort <rjhilgefort@gmail.com>
2022-04-18 19:34:54 -0600

    initial wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm';
+
+return {
+  color_scheme = "Snazzy",
+
+  window_background_opacity = 0.80,
+
+  font_size = 14.0,
+  font = wezterm.font_with_fallback({ "Dank Mono" }),
+
+  scrollback_lines = 3500,
+}
