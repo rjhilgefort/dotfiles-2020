eac9f52e63048dd3de28fb657c3ae05c373b990c
Rob Hilgefort <rjhilgefort@gmail.com>
2025-04-17 21:04:32 -0600

    wezterm updates

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,13 +1,51 @@
-local wezterm = require 'wezterm';
+local wezterm = require 'wezterm'
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+local config = wezterm.config_builder()
 
-return {
-  color_scheme = "Snazzy",
+-- local theme = "Earthsong"
+local theme = "Snazzy"
 
-  window_background_opacity = 0.80,
-  tab_bar_at_bottom = true,
+config.color_scheme = theme
+config.window_background_opacity = 0.80
+config.tab_bar_at_bottom = true
+config.font_size = 14.0
+config.font = wezterm.font_with_fallback({ "Dank Mono" })
+config.scrollback_lines = 14000
 
-  font_size = 14.0,
-  font = wezterm.font_with_fallback({ "Dank Mono" }),
+-- https://github.com/michaelbrusegard/tabline.wez
+tabline.setup({
+  options = { 
+    theme = theme,
+    -- section_separators = {
+    --   left = wezterm.nerdfonts.ple_right_half_circle_thick,
+    --   right = wezterm.nerdfonts.ple_left_half_circle_thick,
+    -- },
+    -- component_separators = {
+    --   left = wezterm.nerdfonts.ple_right_half_circle_thin,
+    --   right = wezterm.nerdfonts.ple_left_half_circle_thin,
+    -- },
+    -- tab_separators = {
+    --   left = wezterm.nerdfonts.ple_right_half_circle_thick,
+    --   right = wezterm.nerdfonts.ple_left_half_circle_thick,
+    -- },
+  },
+  sections = {
+    tabline_a = { 'mode' },
+    tabline_b = { 'workspace' },
+    tabline_c = { ' ' },
+    tab_active = {
+      'index',
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = { 'ram', 'cpu' },
+    tabline_y = { 'datetime', 'battery' },
+    tabline_z = { 'domain' },
+  },
+})
+tabline.apply_to_config(config)
 
-  scrollback_lines = 14000,
-}
+return config
