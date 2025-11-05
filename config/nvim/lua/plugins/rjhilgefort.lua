c96ef6fe11e9465561609dca5cde9b305da8ed1d
Rob Hilgefort <rjhilgefort@gmail.com>
2025-11-05 07:07:31 -0700

    add claude settings

---
+++ config/nvim/lua/plugins/rjhilgefort.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+return {
+  "andweeb/presence.nvim",
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+  },
+}
